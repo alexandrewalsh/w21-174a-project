d0978ce4dfe305aa5800fc743a54173a70ab8f95
Bradley <pickbradley23@gmail.com>
2021-03-10 17:18:12 -0800

    Make player motion easier to adjust

--- player.js
+++ player.js
@@ -5,12 +5,18 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
-const DEF_X = 2;
+const DEF_X = 1.7;
 const DEF_Y = 1.5;
+const DEF_Z = 3;
+
+const JUMP_H = 5;
+
+const MOVE_T = 0.25;
+const JUMP_T = 0.5;
 
 export class Player {
     constructor() {
-        this.player_transform = Mat4.identity().times(Mat4.translation(DEF_X, DEF_Y, 0));
+        this.player_transform = Mat4.identity().times(Mat4.translation(DEF_X, DEF_Y, DEF_Z));
         this.player_moving = false;
         this.player_jumping = false;
 
@@ -53,9 +59,9 @@ export class Player {
         if (this.player_lane == "l") {
             dir = 1;
         }
-        if (time < 0.25) {
+        if (time < MOVE_T) {
             // Player moves sideways with linear speed
-            let x_pos = dir * (16*time - 2);
+            let x_pos = dir * (2 * DEF_X * time / MOVE_T - DEF_X);
             return Mat4.identity().times(Mat4.translation(x_pos, 0, 0));
         }
         else {
@@ -73,9 +79,10 @@ export class Player {
     // Gets an input 'time' and calculates the location of the player through the jump animation
     // If the animation has completed then return player_jumping and started_jump to the default state
     getY(time) {
-        if (time < 0.5) {
+        if (time < JUMP_T) {
             // Player jumps in parabolic motion
-            let y_pos = -Math.pow(8*time - 2, 2) + 4;
+            //let y_pos = -Math.pow(8*time - 2, 2) + 4;
+            let y_pos = - (JUMP_H / 4) * Math.pow((4 / JUMP_T) * time - 2, 2) + JUMP_H;
             return Mat4.identity().times(Mat4.translation(0, DEF_Y + y_pos, 0));
         }
         else {
@@ -83,6 +90,10 @@ export class Player {
             this.started_jump = 0;
             return Mat4.identity().times(Mat4.translation(0, DEF_Y, 0));
         }
+    }
+
+    getZ(time) {
+        return Mat4.identity().times(Mat4.translation(0, 0, DEF_Z));
     }
 
     // Called once per 'display' - modifies this.player_transform based on where player is 
@@ -113,7 +124,8 @@ export class Player {
             y_transform = this.getY(t - this.started_jump);
         }
 
-        this.player_transform = x_transform.times(y_transform);
+        this.player_transform = x_transform.times(y_transform)
+                                           .times(this.getZ(t));
         return this.player_transform;
     }
 }
